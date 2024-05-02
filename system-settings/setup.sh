566c17f9efb68bfb93d2d2820c02d4cbb66da68e
Kristian Walsh <kristian.w@montu.com.au>
2024-05-02 12:05:41 +1000

    feat: adds macos custom default commands

--- system-settings/setup.sh
+++ system-settings/setup.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
 
-# Set default folder for storing screenshots
-# Create the directory if it doesn't exist
-mkdir -p ~/Documents/screenshots/
-# Sets it as default location for screenshots
-defaults write com.apple.screencapture location ~/Documents/screenshots/
-
-
 # Install fonts
 # Copy fonts to the Fonts directory
 cp -a ./fonts/. ~/Library/Fonts
 
 # Copy .gitconfig to the home directory
-#cp ./system-settings/.gitconfig ~
+cp ./system-settings/.gitconfig ~
 
 # Copy .zshrc config
-#cp ./system-settings/.zshrc ~
+cp ./system-settings/.zshrc ~
 
 # Set up dev directory
-#mkdir ~/dev
+mkdir ~/dev
 
 # Set up dev env
 volta install node
+
+# MacOS Customisations
+
+## Set default folder for storing screenshots
+## Create the directory if it doesn't exist
+mkdir -p ~/Documents/screenshots/
+
+## Sets it as default location for screenshots
+defaults write com.apple.screencapture location ~/Documents/screenshots/
+
+## Show all hidden files
+defaults write com.apple.finder AppleShowAllFiles TRUE && killall Finder
+
+## Show recent applications in Dock
+defaults write com.apple.dock "show-recents" -bool false
+
+## Tap to click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+
+## Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
